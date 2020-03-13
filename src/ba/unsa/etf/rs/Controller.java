e1200feda5a2c85741bc00d03bc3e4facc32d4ee
dfejzic1 <dfejzic1@etf.unsa.ba>
2020-03-13 13:01:55 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rs/Controller.java
@@ -0,0 +1,17 @@
+
+package ba.unsa.etf.rs;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class Controller {
+
+    @FXML
+    private Label labela;
+
+    public void akcija(ActionEvent event) {
+        labela.setText("Hello World");
+    }
+}
